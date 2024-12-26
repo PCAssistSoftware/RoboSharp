59eeac7538f8e2707c47bb719beb08fd491eb35f
Robert Brenckman <20431767+RFBomb@users.noreply.github.com>
2024-12-26 11:16:31 -0500

    Rework IFileCopierTests
    - Removed StreamedCopierTests (covered in reworked IFileCopierTests)
    
    `IFileCopierTests` Changes:
    - Reorganized  slightly.
    - Added methods to provide the test objects/names for the various tests
    - Made each of the methods a test method that accepts parameters
    - Added test for ensure that the copy operation is in fact correct. This tests for length, attributes, LastWriteTime (ModifyDate), and the MD5 hash to ensure that the copy operation resulted in an identical file at the destination.
    
    - Currently, this proves that CopyFileEx works identical to BFile.CopyTo, while StreamedCopier needs to be fixed.

--- RoboSharp.Extensions.UnitTests/IFileCopierTests.cs
+++ RoboSharp.Extensions.UnitTests/IFileCopierTests.cs
@@ -4,7 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,8 +14,32 @@ namespace RoboSharp.Extensions.Tests
     /// <summary>
     /// Run a test of the <see cref="IFileCopier"/> standard operations via the provided <see cref="IFileCopierFactory"/>
     /// </summary>
-    public static class IFileCopierTests
+    [TestClass]
+    public class IFileCopierTests
     {
+        private class MyFileSource(string path) : IFileSource
+        {
+            public string FilePath { get; set; } = path;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Required for testing - Platform support is tested as well")]
+        private static IEnumerable<object[]> GetCopierFactory()
+        {
+            yield return Wrap(new StreamedCopierFactory() { BufferSize = StreamedCopier.DefaultBufferSize });
+            yield return Wrap(new Windows.CopyFileExFactory() { Options = Windows.CopyFileExOptions.RESTARTABLE }); // run in restartable mode to artificially slow down the copy operation, otherwise cancellation test may fail due to very quick copy times
+        }
+        private static IEnumerable<object[]> GetCopier() // takes the above factories and gets the copier from it
+        {
+            return GetCopierFactory().Select(o => Wrap(
+                ((IFileCopierFactory)o[0]).Create(GetRandomPath(false), GetRandomPath(true))
+            ));
+        }
+
+        private static object[] Wrap(params object[] objects) => objects;
+        public static String GetCopierName(MethodInfo info, object[] objects) => objects[0].GetType().Name;
+
+        private static string GetRandomPath(bool isSubFolder = false) => TestPrep.GetRandomPath(isSubFolder);
+
         public static void CreateDummyFile(string filePath, int lengthInBytes)
         {
             using (var fileStream = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write, FileShare.None))
@@ -24,8 +49,6 @@ namespace RoboSharp.Extensions.Tests
                 fileStream.Write(data, 0, lengthInBytes);
             }
         }
-
-        private static string GetRandomPath(bool inSubFolder = false) => TestPrep.GetRandomPath(inSubFolder);
 
         public static void PrepSourceAndDest(IFileCopier copier, bool deleteDest = true)
         {
@@ -38,6 +61,7 @@ namespace RoboSharp.Extensions.Tests
                 copier.Source.Refresh();
             }
             if (deleteDest && File.Exists(copier.Destination.FullName)) copier.Destination.Delete();
+            copier.Destination.Directory.Create();
         }
 
         public static async Task Cleanup(IFileCopier copier, bool deleteSource = true)
@@ -55,23 +79,45 @@ namespace RoboSharp.Extensions.Tests
                 copier.Destination.Directory.Delete(false);
         }
 
-        /// <summary>
-        /// Run all tests on the <paramref name="factory"/>
-        /// </summary>
-        /// <param name="factory"></param>
-        /// <returns>The <see cref="IFileCopier"/> that can be used for additional testing if required</returns>
-        public static async Task<IFileCopier> RunTest(IFileCopierFactory factory)
+        public static async Task<bool> ThrowsIfNotWindowsPlatform(IFileCopier copier)
         {
-            IFileCopier copier = RunFactoryTests(factory);
-            await CopyAsyncTest(copier);
-            await MoveAsyncTest(copier);
-            return copier;
+#if NET5_0_OR_GREATER
+            if (VersionManager.IsPlatformWindows)
+            {
+                // do nothing
+            }
+            else if (copier.GetType().GetCustomAttribute(typeof(System.Runtime.Versioning.SupportedOSPlatformAttribute)) is System.Runtime.Versioning.SupportedOSPlatformAttribute attr)
+            {
+                if (attr.PlatformName.StartsWith("windows"))
+                {
+                    await Assert.ThrowsExceptionAsync<PlatformNotSupportedException>(copier.CopyAsync, "\r\n failed to throw PlatformNotSupported");
+                    return false;
+                }
+            }
+#endif
+            await Task.CompletedTask; // ignores 'async but not awaited' warning CS1998
+            return true;
+        }
+
+        /// <summary>
+        /// Runs the various tests against a specific factory
+        /// </summary>
+        internal static async Task RunTests(IFileCopierFactory factory)
+        {
+            var runner = new IFileCopierTests();
+            runner.RunFactoryTests(factory);
+            IFileCopier copier = factory.Create(GetRandomPath(false), GetRandomPath(true));
+            await runner.CopyAsyncTest(copier);
+            await runner.MoveAsyncTest(copier);
+            await runner.AttributesCopiedProperlyTest(copier);
         }
 
         /// <summary>
         /// Tests the basic functionality of an <see cref="IFileCopierFactory"/>
         /// </summary>
-        private static IFileCopier RunFactoryTests(IFileCopierFactory factory)
+        [DynamicData(nameof(GetCopierFactory), dynamicDataSourceType:DynamicDataSourceType.Method, DynamicDataDisplayName = nameof(GetCopierName))]
+        [TestMethod]
+        public void RunFactoryTests(IFileCopierFactory factory)
         {
             Console.WriteLine($"IFileCopierFactory Type : {factory.GetType()}");
             Test_Setup.PrintEnvironment();
@@ -96,17 +142,14 @@ namespace RoboSharp.Extensions.Tests
             Assert.IsNotNull(cp = factory.Create(source.FullName, dest.FullName));
             Assert.AreEqual(source.Name, cp.Source.Name, "\n --- Created source does not match expected file name");
             Assert.AreEqual(dest.Name, cp.Destination.Name, "\n --- Created destination does not match expected file name");
-
-            Console.WriteLine($"IFileCopier Created : {cp.GetType()}");
-            return cp;
-        }
-        private class MyFileSource : IFileSource
-        {
-            public MyFileSource(string path) { FilePath = path; }
-            public string FilePath { get; set; }
         }
 
-        private static async Task CopyAsyncTest(IFileCopier copier)
+        /// <summary>
+        /// Tests the basic functionality of an <see cref="IFileCopier.CopyAsync()"/>
+        /// </summary>
+        [DynamicData(nameof(GetCopier), dynamicDataSourceType: DynamicDataSourceType.Method, DynamicDataDisplayName = nameof(GetCopierName))]
+        [TestMethod]
+        public async Task CopyAsyncTest(IFileCopier copier)
         {
             double progress = 0;
             var tcs = new TaskCompletionSource<object>();
@@ -115,6 +158,9 @@ namespace RoboSharp.Extensions.Tests
             try
             {
                 string destPath = copier.Destination.FullName;
+
+                // check platform support
+                if (await ThrowsIfNotWindowsPlatform(copier) is false) return;
 
                 //Source is missing
                 await Assert.ThrowsExceptionAsync<FileNotFoundException>(copier.CopyAsync, "\n --Did not throw when source is missing \n");
@@ -205,12 +251,20 @@ namespace RoboSharp.Extensions.Tests
 
         }
 
-        public static async Task MoveAsyncTest(IFileCopier copier)
+        /// <summary>
+        /// Tests the basic functionality of an <see cref="IFileCopier.MoveAsync()"/>
+        /// </summary>
+        [DynamicData(nameof(GetCopier), dynamicDataSourceType: DynamicDataSourceType.Method, DynamicDataDisplayName = nameof(GetCopierName))]
+        [TestMethod]
+        public async Task MoveAsyncTest(IFileCopier copier)
         {
             // Move used the Copy api, then checks for completion, and deletes source file if OK
             void PrepMove() => PrepSourceAndDest(copier, false);
             try
             {
+                // check platform support
+                if (await ThrowsIfNotWindowsPlatform(copier) is false) return;
+
                 //Source is missing
                 await Assert.ThrowsExceptionAsync<FileNotFoundException>(() => copier.MoveAsync(), "\n --Did not throw when source is missing \n");
 
@@ -253,6 +307,106 @@ namespace RoboSharp.Extensions.Tests
             {
                 await Cleanup(copier);
                 TestPrep.CleanAppData();
+            }
+        }
+
+        /// <summary>
+        /// Tests that attributes and file itself are copied to the destination file properly, just like if they were copied via File.CopyTo();
+        /// </summary>
+        [DynamicData(nameof(GetCopier), dynamicDataSourceType: DynamicDataSourceType.Method, DynamicDataDisplayName = nameof(GetCopierName))]
+        [TestMethod]
+        public async Task AttributesCopiedProperlyTest(IFileCopier copier)
+        {
+            string fileCopyToDest = copier.Destination.FullName + "_control";
+
+            string? sourceMD5 = null; string? destinationMD5 = null; string controlMD5 = null;
+
+            try
+            {
+                // check platform support
+                if (await ThrowsIfNotWindowsPlatform(copier) is false) return;
+
+                PrepSourceAndDest(copier, true);
+                sourceMD5 = CalculateMD5(copier.Source.FullName);
+
+                File.Copy(copier.Source.FullName, fileCopyToDest, true);
+                FileInfo control = new(fileCopyToDest);
+                await copier.CopyAsync();
+                FileInfo dest = copier.Destination;
+                dest.Refresh();
+
+                // Validate File.CopyTo functionality (ensure test is valid)
+                Assert.AreEqual(copier.Source.Length, control.Length, "\r\n control.Length != source.Length");
+                Assert.AreEqual(copier.Source.Attributes, control.Attributes, "\r\n control.Attributes != source.Attributes");
+
+                Assert.AreEqual(copier.Source.LastWriteTime, control.LastWriteTime, "\r\n control.LastWriteTime != source.LastWriteTime");
+                Assert.AreEqual(copier.Source.LastWriteTimeUtc, control.LastWriteTimeUtc, "\r\n control.LastWriteTimeUtc != source.LastWriteTimeUtc");
+
+                controlMD5 = CalculateMD5(fileCopyToDest);
+                Assert.AreEqual(sourceMD5, controlMD5, "\r\n control.MD5 != source.MD5");
+
+
+#if NET8_0_OR_GREATER
+                Assert.AreEqual(copier.Source.UnixFileMode, dest.UnixFileMode, "\r\n control UnixFileMode  != source!");
+#endif
+
+                // Begin validation
+                Assert.AreEqual(control.Length, dest.Length, "\r\n ifileCopier.Length != File.CopyTo.Length");
+                Assert.AreEqual(control.Attributes, dest.Attributes, "\r\n ifileCopier.Attributes != File.CopyTo.Attributes");
+
+                Assert.AreEqual(control.LastWriteTime, dest.LastWriteTime, "\r\n ifileCopier.LastWriteTime != File.CopyTo.LastWriteTime");
+                Assert.AreEqual(control.LastWriteTimeUtc, dest.LastWriteTimeUtc, "\r\n ifileCopier.LastWriteTimeUtc != File.CopyTo.LastWriteTimeUtc");
+
+                destinationMD5 = CalculateMD5(copier.Destination.FullName);
+                Assert.AreEqual(sourceMD5, destinationMD5, "\r\n ifileCopier.MD5 != source.MD5");
+
+#if NET8_0_OR_GREATER
+                Assert.AreEqual(control.UnixFileMode, dest.UnixFileMode, "\r\n UnixFileMode does not match!");
+#endif
+            }
+            catch
+            {
+                FileInfo c = new(fileCopyToDest);
+                string dateTimeMs = "yyyy/MM/dd hh:mm:ss.fff tt";
+                Console.WriteLine("-----"); 
+                Console.WriteLine($"Source      Length: {copier.Source.Length}");
+                Console.WriteLine($"File.CopyTo Length: {c.Length}");
+                Console.WriteLine($"IFileCopier Length: {copier.Destination.Length}");
+                Console.WriteLine("-----");
+                Console.WriteLine($"Source      Attributes: {copier.Source.Attributes}");
+                Console.WriteLine($"File.CopyTo Attributes: {c.Attributes}");
+                Console.WriteLine($"IFileCopier Attributes: {copier.Destination.Attributes}");
+                Console.WriteLine("-----");
+                Console.WriteLine($"Source      LastWriteTimeUTC: {copier.Source.LastWriteTimeUtc.ToString(dateTimeMs)}");
+                Console.WriteLine($"File.CopyTo LastWriteTimeUTC: {c.LastWriteTimeUtc.ToString(dateTimeMs)}");
+                Console.WriteLine($"IFileCopier LastWriteTimeUTC: {copier.Destination.LastWriteTimeUtc.ToString(dateTimeMs)}");
+                Console.WriteLine("-----");
+                Console.WriteLine($"Source      MD5: {sourceMD5 ?? CalculateMD5(copier.Source.FullName)}");
+                Console.WriteLine($"File.CopyTo MD5: {controlMD5 ?? CalculateMD5(c.FullName)}");
+                Console.WriteLine($"IFileCopier MD5: {destinationMD5 ?? CalculateMD5(copier.Destination.FullName)}");
+                throw;
+            }
+            finally
+            {
+                if (File.Exists(fileCopyToDest)) File.Delete(fileCopyToDest);
+                await Cleanup(copier);
+                TestPrep.CleanAppData();
+            }
+
+            static string CalculateMD5(string filename)
+            {
+                if (File.Exists(filename))
+                {
+                    using (var md5 = MD5.Create())
+                    {
+                        using (var stream = File.OpenRead(filename))
+                        {
+                            var hash = md5.ComputeHash(stream);
+                            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                        }
+                    }
+                }
+                return null;
             }
         }
     }
